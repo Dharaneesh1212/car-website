44540325eb5232121a8946bed1e78418a3f82dec
dharaneesh1212 <dharun12122002@gmail.com>
2024-07-05 11:02:45 +0530

    mongoose

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 app.get("/", (req, res) => {
   res.send("running successfully");
 });
 
-app.listen(PORT, () => {
-  console.log(`server is running in http://localhost:${PORT}`);
-});
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running in http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("error on connecting to database", err);
+  });
