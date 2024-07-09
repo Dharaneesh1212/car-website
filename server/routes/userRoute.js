e150e2ec515544094357cf286ae927b49a03f348
dharaneesh1212 <dharun12122002@gmail.com>
2024-07-09 10:18:04 +0530

    signup

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,4 +1,24 @@
 import express from "express";
-import User from "../models/userModels.js";
+import { User } from "../models/userModels.js";
 
 const router = express.Router();
+
+// signup
+router.post("signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    return res.json({ message: "user already exist" });
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashPassword,
+  });
+
+  await newUser.save();
+  return res.json({status:true ,message:"user created successfully"})
+});
+
