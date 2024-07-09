a65d79323d98f4d1fad330c83b79b85154e241d8
dharaneesh1212 <dharun12122002@gmail.com>
2024-07-09 10:54:48 +0530

    signin

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { User } from "../models/userModels.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 // signup
-router.post("signup", async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
@@ -19,6 +21,34 @@ router.post("signup", async (req, res) => {
   });
 
   await newUser.save();
-  return res.json({status:true ,message:"user created successfully"})
+  return res.json({ status: true, message: "user created successfully" });
 });
 
+// signin
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.json({ message: "user not registered" });
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+
+  if (!validPassword) {
+    return res.json({ message: "password is incorrect" });
+  }
+
+  const token = jwt.sign({ username: user.username }, process.env.KEY, {
+    expiresIn: "1h",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true, // Requires HTTPS
+    sameSite: none,
+    maxAge: 360000, // Adjust based on your session timeout
+  });
+
+  return res.json({ status: true, message: "loged in successfully" });
+});
