6e2e94bd7852f414d57fadaf01d994b30d5dd8c2
dharaneesh1212 <dharun12122002@gmail.com>
2024-07-15 13:25:59 +0530

    no-amc

--- server/routes/serviceRoute.js
+++ server/routes/serviceRoute.js
@@ -11,28 +11,21 @@ const router = express.Router();
 
 // create
 // POST => http://localhost:8000/api/v1/service
-// POST => http://localhost:8000/api/v1/amc
 router.post("/service", createService);
-router.post("/amc", createService);
 
 // get
 // GET => http://localhost:8000/api/v1/sevice
-// GET => http://localhost:8000/api/v1/amc
 router.get("/service",allServices);
-router.get("amc", allServices);
+
 // GET => http://localhost:8000/api/v1/sevice
-// GET => http://localhost:8000/api/v1/amc
 router.get("/service",serviceByName);
-router.get("amc", serviceByName);
 
 // update
 // PUT => http://localhost:8000/api/v1/service/id
-// PUT => http://localhost:8000/api/v1/amc/id
 router.put("/:id", updateServiceId);
 
 // delete
 // DELETE => http://localhost:8000/api/v1/service/id
-// DELETE => http://localhost:8000/api/v1/amc/id
 router.delete("/:id", deleteServiceId);
 
 export default router;
