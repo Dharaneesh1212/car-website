bdaa21deb6f267176553bb3b359dc27aeed6a33b
dharaneesh1212 <dharun12122002@gmail.com>
2024-07-15 09:51:25 +0530

    get-route

--- server/routes/serviceRoute.js
+++ server/routes/serviceRoute.js
@@ -2,6 +2,8 @@ import {
   createService,
   updateServiceId,
   deleteServiceId,
+  allServices,
+  serviceByName,
 } from "../controller/serviceController.js";
 import express from "express";
 
@@ -12,6 +14,16 @@ const router = express.Router();
 // POST => http://localhost:8000/api/v1/amc
 router.post("/service", createService);
 router.post("/amc", createService);
+
+// get
+// GET => http://localhost:8000/api/v1/sevice
+// GET => http://localhost:8000/api/v1/amc
+router.get("/service",allServices);
+router.get("amc", allServices);
+// GET => http://localhost:8000/api/v1/sevice
+// GET => http://localhost:8000/api/v1/amc
+router.get("/service",serviceByName);
+router.get("amc", serviceByName);
 
 // update
 // PUT => http://localhost:8000/api/v1/service/id
