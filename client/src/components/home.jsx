eebe65499cce29abac93cbb9f03b23ff84343fa4
dharaneesh1212 <dharun12122002@gmail.com>
2024-07-26 13:38:16 +0530

    img

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,9 +1,13 @@
+import chvy from "../assets/images/chvy.jpg";
+
 const Home = () => {
-    return ( 
-        <main>
-        
-        </main>
-     );
-}
- 
+  return (
+    <main>
+      <div>
+        <img src={chvy} alt="" />
+      </div>
+    </main>
+  );
+};
+
 export default Home;
