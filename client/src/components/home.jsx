55260a82e23cb428e939bdc9b65b14d4b33d9eb1
dharaneesh1212 <dharun12122002@gmail.com>
2024-07-28 13:06:04 +0530

    zoomin-anim

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -5,14 +5,14 @@ const Home = () => {
       className="bg-black h-screen flex items-center justify-center relative w-full  "
     >
       <div className="flex items-start justify-center flex-col font-semibold text-5xl text-white absolute top-[14rem] left-[4rem] gap-8">
-        <div className="flex items-start justify-center flex-col gap-4">
+        <div className="animate__animated animate__zoomIn flex items-start justify-center flex-col gap-4">
           <h1>
             Your <span className="text-red-600">Trusted</span> Auto
           </h1>
           <h1>Repair Service</h1>
           <h1>Provider</h1>
         </div>
-        <div className="flex items-start justify-center flex-col font-normal text-xl gap-1">
+        <div className="animate__animated animate__zoomIn flex items-start justify-center flex-col font-normal text-xl gap-1">
           <p>We offer reliable and efficient services to ensure your</p>
           <p>vehicle is always in top condition. Let us take care of </p>
           <p>your car, So you can focus on what matters most.</p>
--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -15,23 +15,23 @@ const Register = () => {
           }`}
         >
           <form className="flex items-center justify-center flex-col gap-8 w-[25rem]">
-            <h1 className="text-2xl font-semibold font-mono">Create Account</h1>
+            <h1 className="animate__animated animate__zoomIn text-2xl font-semibold font-mono">Create Account</h1>
             <input
               type="text"
               placeholder="Name"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
             <input
               type="email"
               placeholder="Email"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
             <input
               type="password"
               placeholder="Password"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
-            <button className="flex items-center justify-center h-10 w-[10rem] bg-red-500 rounded-md font-sans font-medium text-lg">
+            <button className="animate__animated animate__zoomIn flex items-center justify-center h-10 w-[10rem] bg-red-500 rounded-md font-sans font-medium text-lg">
               Sign Up
             </button>
           </form>
@@ -42,18 +42,18 @@ const Register = () => {
           }`}
         >
           <form className="flex items-center justify-center flex-col gap-10 w-[25rem]">
-            <h1 className="text-2xl font-semibold font-mono">Sign In</h1>
+            <h1 className="animate__animated animate__zoomIn text-2xl font-semibold font-mono">Sign In</h1>
             <input
               type="email"
               placeholder="Email"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
             <input
               type="password"
               placeholder="Password"
-              className="h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
+              className="animate__animated animate__zoomIn h-8 w-72 bg-slate-100 p-1 rounded-md font-sans font-medium text-lg outline-none"
             />
-            <button className="flex items-center justify-center h-10 w-[10rem] bg-blue-500 rounded-md font-sans font-medium text-lg">
+            <button className="animate__animated animate__zoomIn flex items-center justify-center h-10 w-[10rem] bg-blue-500 rounded-md font-sans font-medium text-lg">
               Sign In
             </button>
           </form>
@@ -65,14 +65,14 @@ const Register = () => {
                 activeTab === "sign-in" ? "" : "hidden"
               }`}
             >
-              <h1 className="text-3xl font-semibold font-mono">
+              <h1 className="animate__animated animate__zoomIn text-3xl font-semibold font-mono">
                 Welcome Back !
               </h1>
-              <p className="text-xl font-small font-mono">
+              <p className="animate__animated animate__zoomIn text-xl font-small font-mono">
                 Enter your personal details to use all site features
               </p>
               <button
-                className="flex items-center justify-center h-10 w-[8rem] bg-transparent rounded-md font-sans font-medium text-lg text-white shadow-[0_5px_15px_rgba(0,0,0,0.35)]"
+                className="animate__animated animate__zoomIn flex items-center justify-center h-10 w-[8rem] bg-transparent rounded-md font-sans font-medium text-lg text-white shadow-[0_5px_15px_rgba(0,0,0,0.35)]"
                 onClick={handleTabToggle}
               >
                 Sign Up
@@ -83,14 +83,14 @@ const Register = () => {
                 activeTab === "sign-up" ? "" : "hidden"
               }`}
             >
-              <h1 className="text-3xl font-semibold font-mono">
+              <h1 className="animate__animated animate__zoomIn text-3xl font-semibold font-mono">
                 Welcome Friend !
               </h1>
-              <p className="text-xl font-small font-mono">
+              <p className="animate__animated animate__zoomIn text-xl font-small font-mono">
                 Enter your personal details to use all site features
               </p>
               <button
-                className="flex items-center justify-center h-10 w-[8rem] bg-transparent rounded-md font-sans font-medium text-lg text-white shadow-[0_5px_15px_rgba(0,0,0,0.35)]"
+                className="animate__animated animate__zoomIn flex items-center justify-center h-10 w-[8rem] bg-transparent rounded-md font-sans font-medium text-lg text-white shadow-[0_5px_15px_rgba(0,0,0,0.35)]"
                 onClick={handleTabToggle}
               >
                 Sign In
