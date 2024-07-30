090fb528d3006826f29189fbad8187c8ed522234
dharaneesh1212 <dharun12122002@gmail.com>
2024-07-30 14:36:58 +0530

    id

--- client/src/components/amc.jsx
+++ client/src/components/amc.jsx
@@ -1,44 +1,59 @@
 const Amc = () => {
   return (
-    <main id="amc" className="bg-black w-screen h-screen flex items-center flex-col justify-center gap-4">
+    <main
+      id="amc"
+      className="bg-black w-screen h-screen flex items-center flex-col justify-center gap-4"
+    >
       <div className="flex items-center justify-start flex-col gap-10 h-[20rem]">
         <div className="flex items-center justify-center gap-4">
           <input
+            id="amc-name"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Person Name"
           />
           <input
+            id="amc-number"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Car Number"
           />
           <input
+            id="amc-car"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Car Name"
           />
           <input
+            id="amc-comp"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Complaint"
           />
           <input
+            id="amc-status"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Complaint"
           />
         </div>
-        <button className="animate__animated animate__zoomIn bg-red-700 text-xl text-white outline-none p-2 rounded-md h-[3rem] w-[10rem]">
+        <button
+          id="amc-reg"
+          className="animate__animated animate__zoomIn bg-red-700 text-xl text-white outline-none p-2 rounded-md h-[3rem] w-[10rem]"
+        >
           Register
         </button>
         <div className="flex items-center justify-center gap-4">
           <input
+            id="amc-search"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Search By Name "
           />
-          <button className="animate__animated animate__zoomIn bg-red-700 text-xl text-white outline-none p-2 rounded-md h-[3rem] w-[10rem]">
+          <button
+            id="amc-btn"
+            className="animate__animated animate__zoomIn bg-red-700 text-xl text-white outline-none p-2 rounded-md h-[3rem] w-[10rem]"
+          >
             Search
           </button>
         </div>
--- client/src/components/service.jsx
+++ client/src/components/service.jsx
@@ -1,44 +1,59 @@
 const Service = () => {
   return (
-    <main id="service" className="bg-black w-screen h-screen flex items-center flex-col justify-center gap-4">
+    <main
+      id="service"
+      className="bg-black w-screen h-screen flex items-center flex-col justify-center gap-4"
+    >
       <div className="flex items-center justify-start flex-col gap-10 h-[20rem]">
         <div className="flex items-center justify-center gap-4">
           <input
+            id="service-name"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Person Name"
           />
           <input
+            id="service-number"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Car Number"
           />
           <input
+            id="service-car"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Car Name"
           />
           <input
+            id="service-comp"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Complaint"
           />
           <input
+            id="service-status"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Complaint"
           />
         </div>
-        <button className="animate__animated animate__zoomIn bg-red-700 text-xl text-white outline-none p-2 rounded-md h-[3rem] w-[10rem]">
+        <button
+          id="service-reg"
+          className="animate__animated animate__zoomIn bg-red-700 text-xl text-white outline-none p-2 rounded-md h-[3rem] w-[10rem]"
+        >
           Register
         </button>
         <div className="flex items-center justify-center gap-4">
           <input
+            id="service-search"
             className="animate__animated animate__zoomIn rounded-md p-2 text-xl outline-none text-white bg-zinc-700"
             type="text"
             placeholder="Search By Name "
           />
-          <button className="animate__animated animate__zoomIn bg-red-700 text-xl text-white outline-none p-2 rounded-md h-[3rem] w-[10rem]">
+          <button
+            id="service-btn"
+            className="animate__animated animate__zoomIn bg-red-700 text-xl text-white outline-none p-2 rounded-md h-[3rem] w-[10rem]"
+          >
             Search
           </button>
         </div>
