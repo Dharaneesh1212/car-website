4d398b2a4ab4a396e87839ee246ab61d278b2445
dharaneesh1212 <dharun12122002@gmail.com>
2024-08-01 13:42:56 +0530

    responsive-rp

--- client/src/components/resetPassword.jsx
+++ client/src/components/resetPassword.jsx
@@ -1,8 +1,8 @@
 const ResetPassword = () => {
   return (
     <main className="flex items-center justify-center h-screen bg-black">
-      <div className="flex items-center justify-evenly flex-row bg-zinc-900 h-[25rem] w-[25rem] rounded-3xl shadow-[0_5px_15px_rgba(0,0,0,0.35)]">
-        <div id="reset-password" className="flex items-center justify-center">
+      <div id="reset-password" className="flex items-center justify-evenly flex-row bg-zinc-900 h-[25rem] w-[25rem] rounded-3xl shadow-[0_5px_15px_rgba(0,0,0,0.35)]">
+        <div className="flex items-center justify-center">
           <form className="flex items-center justify-center flex-col gap-8 w-[25rem]">
             <h1 className="text-2xl font-semibold font-mono text-white">
               Reset Password
