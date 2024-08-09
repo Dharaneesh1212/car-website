6e507e45de368fd943af7630e39c238db68a3583
dharaneesh1212 <dharun12122002@gmail.com>
2024-08-09 11:45:59 +0530

    navbar-responsive

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,59 +1,79 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { MdHome } from "react-icons/md";
+import { MdHome, MdContactMail, MdMenu, MdClose } from "react-icons/md";
 import { GrGallery } from "react-icons/gr";
 import { BiSolidCarMechanic } from "react-icons/bi";
-import { FaCrown } from "react-icons/fa6";
-import { MdContactMail } from "react-icons/md";
-import { FaUser } from "react-icons/fa";
+import { FaCrown, FaUser } from "react-icons/fa";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <main id="navbar" className="w-screen h-[5rem] fixed flex items-center justify-between p-10 z-10">
-      <div className="font-semibold text-xl text-white">
+    <main id="navbar" className="w-screen fixed flex items-center justify-between p-4 z-10 bg-transparent">
+      <div className="font-semibold text-xl text-white animate__animated animate__zoomIn">
         <h1>
           AUT<span className="text-red-600">O</span>W
           <span className="text-red-600">O</span>RKS
         </h1>
       </div>
-      <div className="flex items-center justify-center gap-7">
+      <div className="lg:hidden">
+        <button onClick={toggleMenu} className="text-white text-3xl">
+          {isOpen ? <MdClose /> : <MdMenu />}
+        </button>
+      </div>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } absolute top-[5rem] left-0 w-full bg-black flex flex-col items-center gap-4 lg:flex lg:flex-row lg:static lg:top-0 lg:w-auto lg:bg-transparent`}
+      >
         <Link
           to="/"
-          className="flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          className="animate__animated animate__zoomIn flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          onClick={() => setIsOpen(false)}
         >
           <MdHome />
           HOME
         </Link>
         <Link
           to="/gallery"
-          className="flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          className="animate__animated animate__zoomIn flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          onClick={() => setIsOpen(false)}
         >
           <GrGallery />
           GALLERY
         </Link>
         <Link
           to="/service"
-          className="flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          className="animate__animated animate__zoomIn flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          onClick={() => setIsOpen(false)}
         >
           <BiSolidCarMechanic />
           SERVICE
         </Link>
         <Link
           to="/amc"
-          className="flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          className="animate__animated animate__zoomIn flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          onClick={() => setIsOpen(false)}
         >
           <FaCrown />
           AMC
         </Link>
         <Link
           to="/contact"
-          className="flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          className="animate__animated animate__zoomIn flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          onClick={() => setIsOpen(false)}
         >
           <MdContactMail />
           CONTACT
         </Link>
         <Link
           to="/register"
-          className="flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          className="animate__animated animate__zoomIn flex justify-center items-center gap-1 text-xl text-white hover:text-red-600"
+          onClick={() => setIsOpen(false)}
         >
           <FaUser />
           REGISTER
