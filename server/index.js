d18a835b33157b25364eb8f50b69eb47b570f65b
dharaneesh1212 <dharun12122002@gmail.com>
2024-08-10 13:45:57 +0530

    cookie

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const CONNECTION_URL = process.env.CONNECTION_URL;
 // middleware
 app.use(express.json());
 app.use(cors({
-  origin:["http://localhost:5173"],
+  origin:"http://localhost:5173",
   credentials:true
 }));
 app.use(cookieParser());
