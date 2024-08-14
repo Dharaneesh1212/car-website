ddf346e761a804bfa18d5af382b5c22be0cbe069
dharaneesh1212 <dharun12122002@gmail.com>
2024-08-14 15:04:43 +0530

    complete

--- admin/src/components/context.jsx
+++ admin/src/components/context.jsx
@@ -2,13 +2,14 @@ import { createContext } from "react";
 
 export const StoreContext = createContext();
 
-const Context = ({children}) => {
-    const url = "http://localhoste:8000"
-    return ( 
-        <StoreContext.Provider>
+const Context = ({ children }) => {
+    const url = "http://localhost:8000"; 
+
+    return (
+        <StoreContext.Provider value={{ url }}>
             {children}
         </StoreContext.Provider>
-     );
+    );
 }
- 
+
 export default Context;
